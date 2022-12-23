08c76630476522b4bc066968a5b555489cc5e65e
unknown <jonrodriguez@alumno.unlam.edu.ar>
2022-12-23 14:20:13 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,116 @@
+﻿(function ($) {
+  $(document).ready(function () {
+    "use strict";
+
+    /*ScrollR */
+    if ($(window).width() > 1024) {
+      var s = skrollr.init({
+        forceHeight: false
+      });
+    }
+
+    /*Gallery ColorBox */
+    $('.gallery_txt a').colorbox({
+      rel: 'gal',
+      maxWidth: "100%",
+    });
+
+    /*Main Menu Button */
+    $('.main_menu_btn').on("click", function (e) {
+      $(this).toggleClass('main_menu_btn_open');
+      $('.main_menu_block').toggleClass('main_menu_block_open').fadeToggle();
+      $('.main_menu_block').find('.menu_wrapper').toggleClass('active');
+      $('header .anim').toggleClass('active');
+      e.preventDefault();
+    });
+
+    /* Section Background */
+    $('section, .parallax').each(function () {
+      var image = $(this).attr('data-image');
+      if (image) {
+        $(this).css('background-image', 'url(' + image + ')');
+      }
+    });
+
+    /*Scroll Effect*/
+    $('.intro_down, .go').on("click", function (e) {
+      var anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top
+      }, 1000);
+      e.preventDefault();
+    });
+
+    /*Show/Hide Photo in When&Where Block*/
+    $('.photocamera span').on("click", function (e) {
+      $(this).parents('section').find('.opacity').toggleClass('fade');
+      $(this).parents('section').find('.over').fadeToggle();
+      e.preventDefault();
+    });
+
+    /*Player*/
+    $('.music').on("click", function (e) {
+      $('.player').fadeToggle();
+      e.preventDefault();
+    });
+
+    /*CountDown*/
+    $('.married_coundown').countdown({until: new Date("Jan 28, 2023 21:00:00")});
+
+    /*Gallery Carousel */
+    $(".gallery_wrapper").owlCarousel({
+      navigation: true, responsive: true, responsiveRefreshRate: 200, slideSpeed: 200,
+      paginationSpeed: 200, rewindSpeed: 500, items: 3, itemsTablet: [768, 2], autoPlay: true,
+      itemsMobile: [479, 1], mouseDrag: true
+    });
+
+    /*Registry Carousel */
+    $(".registry_wrapper").owlCarousel({
+      navigation: true, responsive: true, responsiveRefreshRate: 200, slideSpeed: 200, paginationSpeed: 200,
+      rewindSpeed: 500, stopOnHover: true, autoHeight: true, items: 3, mouseDrag: true, autoPlay: true
+    });
+
+    /*The Crew Carousel*/
+    $(".guest_wrapper").owlCarousel({
+      navigation: true, responsive: true, responsiveRefreshRate: 200, slideSpeed: 200, paginationSpeed: 200,
+      rewindSpeed: 500, stopOnHover: true, autoHeight: true, items: 4, mouseDrag: true, autoPlay: true
+    });
+
+    /*Slider Carousel*/
+    $(".slider").owlCarousel({
+      navigation: true,
+      responsive: true,
+      responsiveRefreshRate: 200,
+      slideSpeed: 200,
+      paginationSpeed: 200,
+      rewindSpeed: 500,
+      stopOnHover: false,
+      autoHeight: true,
+      singleItem: true,
+      mouseDrag: true,
+      autoPlay: true,
+      transitionStyle: "fade"
+    });
+
+    /* Top Menu Click to Section */
+    $('.sub_menu').find('a').on("click", function (e) {
+      $('.sub_menu').find('a').removeClass('active');
+      $(this).addClass('active');
+      var anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top + 1
+      }, 1000);
+      $(".main_menu_btn").trigger('click');
+      e.preventDefault();
+    });
+
+    /*FireFly in Intro*/
+    $.firefly({
+      color: '#fff', minPixel: 1, maxPixel: 3, total: 55, on: '.into_firefly'
+    });
+
+    /* Refresh ScrollR */
+    s.refresh($(".guest_wrapper, .our_story"));
+
+  });
+}(jQuery));
